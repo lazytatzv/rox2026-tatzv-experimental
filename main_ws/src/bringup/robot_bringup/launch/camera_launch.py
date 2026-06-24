e2ab0d2c36d9ba2d6c6abc38a8fe92f558185614
imakuma-ryoichi <imakuma.ryoichi5957@mail.kyutech.jp>
2026-06-24 19:03:48 +0900

    camera launch file

---
+++ main_ws/src/bringup/robot_bringup/launch/camera_launch.py
@@ -0,0 +1,12 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='v4l2_camera',
+            executable='v4l2_camera_node',
+        )
+    ])
+
